9854396b7c8e04f13aa09f73961a9423e8182a34
Enrique FR <enriquefranciscoramirez95@gmail.com>
2021-10-03 10:47:59 -0500

    JWT implementado

--- controller/usuarios.controller.js
+++ controller/usuarios.controller.js
@@ -76,12 +76,17 @@ const usuariosDelete = async(req, res = response) => {
 
     const { id } = req.params;
 
+    /* const uid = req.uid; */
+    
+
+
     // Fisicamente lo borramos
     /* const usuario = await Usuario.findByIdAndDelete(id); */
 
     const usuario = await Usuario.findByIdAndUpdate( id, { estado: false });
+    const usuarioAutenticado =  req.usuario;
 
-    res.json(usuario);
+    res.json({usuario, usuarioAutenticado});
 }
 
 
--- models/server.js
+++ models/server.js
@@ -8,6 +8,7 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPATH = '/api/usuarios';
+        this.authPath = '/api/auth';
 
         // Conectar a base de datos
         this.conectarDB();
@@ -37,6 +38,7 @@ class Server {
 
     routes() {
 
+        this.app.use( this.authPath, require('../routers/auth'));
         this.app.use( this.usuariosPATH, require('../routers/usuarios'));
     }
 
